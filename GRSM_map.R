abe9c011a1819695a290dc3878b98623d0ec207f
jgradym <jgradym@gmail.com>
2025-09-26 19:37:58 -0400

    Update GRSM_map.R

--- GRSM_map.R
+++ GRSM_map.R
@@ -1,17 +1,13 @@
 
 library(tidyverse)
+library(sf)
 grsm_locations = read_csv('/Users/jgradym/Library/CloudStorage/GoogleDrive-jgradym@gmail.com/Shared drives/GRSM_CESU/Old/Data/MacroInvertebrates/Locations.csv')
 
-
-topo = st_read("/Users/jgradym/Downloads/GRSM_100K_TOPO_BOUNDARIES.geojson")
-boundary = st_read("/Users/jgradym/Downloads/GRSM_100K_TOPO_BOUNDARIES.geojson")
-boundary = st_read("/Users/jgradym/Downloads/GRSM_100K_TOPO_BOUNDARIES/GRSM_100K_TOPO_BOUNDARIES.shp")
-plot(boundary[10])
+watershed = st_read("/Users/jgradym/Downloads/GRSM_WATERSHEDS.geojson")[2]
 watershed = st_read("/Users/jgradym/Downloads/GRSM_WATERSHEDS/GRSM_WATERSHEDS.shp")[2]
 str(watershed)
 plot(watershed)
-# --- deps ---
-library(sf)
+
 
 # start from your 'watershed' object
 ws <- watershed
@@ -55,11 +51,17 @@ pts_first <- pts[!duplicated(pts$StreamName), ]
 pts_first_sf <- sf::st_as_sf(pts_first, coords = c("LON","LAT"), crs = 4326, remove = FALSE)
 
 # draw
-plot(sf::st_geometry(ws), col = NA, border = "black", lwd = 1.8, axes = TRUE)
-plot(sf::st_geometry(pts_first_sf), add = TRUE, pch = 16, cex = 0.9, col = "#1f78b4")
-
+pdf("~/Downloads/GRSM_map2.pdf")
+plot(sf::st_geometry(ws), col = NA, border = "black", lwd = 1, axes = TRUE)
+plot(sf::st_geometry(pts_first_sf), add = TRUE, pch = 16, cex = 1, col = "#1f78b4")
+text(pts_first$LON, pts_first$LAT, labels = pts_first$StreamName, pos = 4, cex = 0.5)
+dev.off()
 # # (optional) labels:
-# text(pts_first$LON, pts_first$LAT, labels = pts_first$StreamName, pos = 4, cex = 0.7)
+# 
+
+neon_field = st_read("~/Downloads/NEON_Field_Sites/NEON_Field_Sites_v17.shp")
+neon_aquatic = neon_field %>% filter(siteType == "Core Aquatic")
+plot(neon_aquatic[3])
 
 # ===== Option B: ONE point per stream = centroid of that stream's sites (use if you prefer) =====
 # by_mean <- aggregate(cbind(LON, LAT) ~ StreamName, data = pts, FUN = mean)
